bbc8e97d8626daef04eb02ca5b0040df8e63d93e
parth-105 <kalathiyaharsh7@gmail.com>
2026-03-11 17:52:49 +0530

    feat: Implement solutions for Reverse String and Valid Palindrome.

--- Q5-125. Valid Palindrome.cpp
+++ Q5-125. Valid Palindrome.cpp
@@ -38,7 +38,22 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(string s) {
-        return false; // Placeholder return
+        int l = 0 , r = s.length()-1;
+
+        while(l<r){
+          while(l<r && !isalnum(s[l])){
+            l++;
+          }
+          while(l<r && !isalnum(s[r])){
+            r--;
+          }
+          if(tolower(s[l]) != tolower(s[r])){
+            return false;
+          }
+          l++;
+          r--;
+        }
+        return true;
     }
 };
 
